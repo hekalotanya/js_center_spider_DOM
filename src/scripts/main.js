bd90211252d4278ca5da99f021324ea0bf5f48ac
hekalotanya <hekalot@gmail.com>
2021-08-23 16:48:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 'use strict';
 
 // write code here
+const wall = document.querySelector('.wall');
+
+wall.style.cssText
+= 'display: flex; justify-content: center; align-items: center';
+
+const spider = document.querySelector('.spider');
+
+spider.style.cssText = 'position: unset';
